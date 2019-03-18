f9558dd7b683bd7ea9ec3341640b30dbefa908d1
Mills Provosty <millsprovosty@gmail.com>
2019-03-18 17:49:52 -0600

    Added test for attributes

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,8 +4,22 @@ require 'pry'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
-  def test_ship_exists
-    cruiser = Ship.new("Cruiser", 3)
-    assert_instance_of Ship, cruiser
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
   end
+
+  def test_ship_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_ship_has_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+  end
+
+  def test_
+
+  end
+
 end
