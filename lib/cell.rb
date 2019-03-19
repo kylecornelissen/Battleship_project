964eb5686bda96d60b1343f01954a7df1c7643fb
Kyle Cornelissen <kyle.cornelissen@gmail.com>
2019-03-18 18:52:26 -0600

    added empty method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,13 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :empty
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @empty = true
   end
 
-
+  def empty?
+    @empty
+  end
 end
