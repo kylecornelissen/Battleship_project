0bf53e6ff69c71c1d94f9a6f0d7eb83a95e42f69
Kyle Cornelissen <kyle.cornelissen@gmail.com>
2019-03-19 15:31:53 -0600

    changed ship and empty methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,20 +1,16 @@
 class Cell
   attr_reader :coordinate,
-              :empty
+              :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
-    @empty = true
+    @ship = nil
   end
 
-  def ship
-    if @empty = true
-      nil
-    end
-  end
+
 
   def empty?
-    @empty
+    @ship
   end
 
   # def place_ship(ship)
