10169c9f5687881f8f2df52036cdfdc4a7390e3e
Mills Provosty <millsprovosty@gmail.com>
2019-03-21 14:02:39 -0600

    Finished overlapped test
    Co-authored-by: Kyle Cornelissen <kyle.cornelissen@gmail.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -38,33 +38,33 @@ class BoardTest < Minitest::Test
 
   def test_if_method_splits_coordinates
 
-    assert_equal ["A", "1", "A", "2"], @board.split_coordinates("A1", "A2")
+    assert_equal ["A", "1", "A", "2"], @board.split_coordinates(["A1", "A2"])
   end
 
   def test_if_letters_can_be_stored_in_letters_array
-    assert_equal ["A", "A"], @board.store_letters("A", "1", "A", "2")
-    assert_equal ["B", "C", "D"], @board.store_letters("B", "1", "C", "1", "D", "1")
+    assert_equal ["A", "A"], @board.store_letters(["A", "1", "A", "2"])
+    assert_equal ["B", "C", "D"], @board.store_letters(["B", "1", "C", "1", "D", "1"])
   end
 
   def test_if_numbers_can_be_stored_in_numbers_array
-    assert_equal [1, 2], @board.store_numbers("A", "1", "A", "2")
-    assert_equal [1, 1, 1], @board.store_numbers("B", "1", "C", "1", "D", "1")
+    assert_equal [1, 2], @board.store_numbers(["A", "1", "A", "2"])
+    assert_equal [1, 1, 1], @board.store_numbers(["B", "1", "C", "1", "D", "1"])
   end
 
   def test_if_letters_can_be_converted_to_ordinals
-    assert_equal [65, 65], @board.letters_to_ordinals("A", "A")
+    assert_equal [65, 65], @board.letters_to_ordinals(["A", "A"])
   end
 
   def test_if_method_detects_consecutive_letters
-    refute @board.consecutive_letters?(65, 65)
-    assert @board.consecutive_letters?(65, 66, 67)
-    refute @board.consecutive_letters?(67, 65)
+    refute @board.consecutive_letters?([65, 65])
+    assert @board.consecutive_letters?([65, 66, 67])
+    refute @board.consecutive_letters?([67, 65])
   end
 
   def test_if_method_detects_consecutive_numbers
-    assert @board.consecutive_numbers?(1, 2)
-    refute @board.consecutive_numbers?(1, 1, 1)
-    refute @board.consecutive_numbers?(3, 1)
+    assert @board.consecutive_numbers?([1, 2])
+    refute @board.consecutive_numbers?([1, 1, 1])
+    refute @board.consecutive_numbers?([3, 1])
   end
 
   def test_if_placement_of_ship_has_consecutive_coordinates
@@ -100,8 +100,9 @@ class BoardTest < Minitest::Test
 
   def test_we_cannot_overlap_ships
     @board.place(@cruiser, ["A1", "A2", "A3"])
-    
+
     refute @board.valid_placement?(@submarine, ["A1", "B1"])
+    assert @board.valid_placement?(@submarine, ["B2", "B3"])
   end
 
 end
