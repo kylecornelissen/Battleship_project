6693f526a4de0d17a25c7d8f6e0532df9f6be02f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-03-25 16:22:38 -0600

    Still writing runner tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -19,4 +19,13 @@ class GameTest < Minitest::Test
   def test_welcome_message
     assert_equal "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.", @game.welcome_message
   end
+
+  def test_computer_places_ship
+    @game.computer_place(@cruiser)
+    assert_equal 3 , @cells.ship.length
+  end
+
+  # def test_player_places_ship
+  #   assert_equal something ,@game.player_place(@cruiser)
+  # end
 end
